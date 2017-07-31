b60a96d4e6810d663b9def5f2ad21ee283568af4
LuKenneth <wannafight1@yahoo.com>
2017-07-31 18:50:33 -0400

    fixed ratio, cursor

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	private GameObject player;
+	public GameObject showMessage;
+	public GameObject hideMessage;
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +17,11 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		player.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.0f));
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			Cursor.visible = !Cursor.visible;
+			showMessage.SetActive(!Cursor.visible);
+			hideMessage.SetActive(Cursor.visible);
+		}
 	}
 
 }
